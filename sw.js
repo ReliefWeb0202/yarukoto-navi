cf1e01b2efa2ae44e38f85955fd0249e4e4e3db6
ReliefWeb0202 <yuka.i020207@gmail.com>
2026-08-17 11:07:23 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+// sw.js (最小限のService Workerファイル)
+const CACHE_NAME = 'ai-task-app-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
